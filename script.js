a78602a31ca4336c19d0b59622c892719ca7d96b
annanehathomas <annanehathomas@gmail.com>
2025-03-01 00:06:11 +0530

    resolved the issue

--- script.js
+++ script.js
@@ -14,26 +14,9 @@ document.addEventListener("keydown", function (event) {
   document.getElementById("keyCombo").textContent = combination.join(" + ");
 
   playSound();
-  updateKeyHistory(combination.join(" + "));
 });
 
 function playSound() {
   let audio = new Audio("keypress.mp3"); // Add a sound file
   audio.play();
-}
-
-let keyHistory = [];
-function updateKeyHistory(key) {
-  let historyDiv = document.getElementById("keyHistory");
-  
-  // Add new key to history
-  keyHistory.unshift(key);
-  
-  // Keep only last 5 keys
-  if (keyHistory.length > 5) keyHistory.pop();
-  
-  // Update UI
-  historyDiv.innerHTML = keyHistory
-      .map(k => `<span class="key-box">${k}</span>`)
-      .join("");
 }
